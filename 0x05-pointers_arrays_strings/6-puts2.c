24824942c7cae1b2cb1b7fe370be70e16a773fcd
LauraYarda <LauraYarda5@gmail.com>
2023-02-27 06:01:33 -0800

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 -  prints every other character of a string
+ * @str: the string to be treated
+ *
+ * Retun: 0
+ */
+void puts2(char *str)
+{
+	int a;
+	int b = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+	for (a = 0; a < b; a += 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}

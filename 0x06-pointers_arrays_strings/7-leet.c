4a9668d8eb2c357d8f42e3a917d4d6c97322c24a
LauraYarda <LauraYarda5@gmail.com>
2023-03-02 07:02:11 -0800

    7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  leet - Encodes a string to 1337
+ *  @str: The string to be encode
+ *
+ *  Return: A pointer to the encoded string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0;str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}

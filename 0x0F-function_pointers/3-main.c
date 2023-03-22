fd4cd19afa31a647c840cdedda30bde4c21f5096
LauraYarda <LauraYarda5@gmail.com>
2023-03-22 09:25:41 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include "3-cal.h"
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
  *
  * Return:  The sum of a and b
  */
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
 	return (a + b);
 }

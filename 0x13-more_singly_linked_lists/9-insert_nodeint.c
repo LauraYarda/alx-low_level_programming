ae97b4712b3c9f8bd6ade60632aebb0592335864
LauraYarda <LauraYarda5@gmail.com>
2023-04-04 02:03:27 -0700

    9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	return (newNode);
 	}
 
-	while (i < idx - 1 && temp->next != NULL)
+	while (i < idx - 1 && temp->next)
 	{
 	temp = temp->next;
 	i++;

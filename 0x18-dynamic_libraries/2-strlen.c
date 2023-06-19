46fcb90f0ff0f44175f5f5a8883677d6e804190f
LauraYarda <Laurayarda5@gmail.com>
2023-06-19 09:48:36 -0700

    libdynamic

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen -  returns the length of a string
+ * @str: the string to get the length of
+ *
+ * Return: the length of @str
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
